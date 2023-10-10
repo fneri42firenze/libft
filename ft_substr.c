1ae1c25b3248b77b853f7d3b35471a08f5103166
fneri42firenze <filippomartinoneri@gmail.com>
2023-10-10 14:21:39 +0200

    a

--- ft_substr.c
+++ ft_substr.c
@@ -1,26 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fneri <fneri@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 14:13:29 by fneri             #+#    #+#             */
+/*   Updated: 2023/10/10 14:19:09 by fneri            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
     size_t  i;
     size_t  j;
+	char *str;
 
-    i == start - 1;
-    char *str;
-    if(!s)
+    i = 0;
+	j = 0;
+    if(!str)
         return (NULL);
-    str = (char *)malloc(sizeof(*str) * (len + 1));
-    while(i = len)
+    str = (char*)malloc(sizeof(*s) * (len + 1));
+    while(s[i])
     {
-        str = (char *)s;
-        i++;
+        if (i >= start && j < len)
+		{
+				str[j] = s[i];
+        	j++;
+		}
+		i++;
     }
+	str[j] = '\0';
     return (str);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 int  main()
 {
-    char    s[] = "";
+    char    s[] = "abcdef";
     printf("%s\n", ft_substr(s, 3, 2));
-}
+}*/
